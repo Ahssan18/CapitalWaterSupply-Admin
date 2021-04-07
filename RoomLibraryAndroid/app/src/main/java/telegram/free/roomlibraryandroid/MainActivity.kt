c2fd2fcbb5aa14bc2acc623deda403b5c4b5bd31
abubakar <abubakar.kaleem@wizlinx.com>
2021-04-07 20:56:41 +0500

    Room Library Practice

---
+++ RoomLibraryAndroid/app/src/main/java/telegram/free/roomlibraryandroid/MainActivity.kt
@@ -0,0 +1,21 @@
+package telegram.free.roomlibraryandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navcontroler: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navcontroler = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navcontroler)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.fragment), null)
+    }
+}
